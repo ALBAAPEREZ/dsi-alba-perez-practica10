67b9a3b616bfd2229b52b7e9f7abb0913683fe5b
Alba Perez <alu0101513768@ull.edu.es>
2024-04-01 17:25:15 +0000

    problemas

--- test/EJERCICIO1.spec.ts
+++ test/EJERCICIO1.spec.ts
@@ -1,7 +1,8 @@
 import 'mocha';
 import {expect} from 'chai';
-import {add} from "../src/EJERCICIO1";
+import {add} from '../src/EJERCICIO1.js';
 
+// PRUEBAS PARA ADD
 describe("add function tests", () => {
   it("add(1, 8) returns value 9", () => {
     expect(add(1, 8)).to.be.equal(9);
